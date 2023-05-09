3943465528cf6887e5f6ed54593778d38d570dc6
Laerone <132922032+Laerone@users.noreply.github.com>
2023-05-09 09:13:45 +0300

    Update Laba 1.cpp

--- Laba 1.cpp
+++ Laba 1.cpp
@@ -2,44 +2,45 @@
 
 class Sandglass {
 private:
-    int a;
-    int b;
+    int a;  // Приватна змінна-член "a"
+
+    int b;  // Приватна змінна-член "b"
 
 public:
     Sandglass() {
-        a = 0;
-        b = 0;
+        a = 0;  // Конструктор за замовчуванням, що встановлює значення "a" на 0
+        b = 0;  // Конструктор за замовчуванням, що встановлює значення "b" на 0
     }
 
     Sandglass(int a, int b) {
-        this->a = a;
-        this->b = b;
+        this->a = a;  // Конструктор, що ініціалізує "a" заданою величиною
+        this->b = b;  // Конструктор, що ініціалізує "b" заданою величиною
     }
 
     void setA(int a) {
-        this->a = a;
+        this->a = a;  // Метод-сетер для оновлення значення "a"
     }
 
     int getA() const {
-        return a;
+        return a;  // Метод-гетер для доступу до значення "a"
     }
 
     void setB(int b) {
-        this->b = b;
+        this->b = b;  // Метод-сетер для оновлення значення "b"
     }
 
     int getB() const {
-        return b;
+        return b;  // Метод-гетер для доступу до значення "b"
     }
 
     int computeArea() const {
-        return 0.5 * a * b;
+        return 0.5 * a * b;  // Метод для обчислення площі
     }
 
     void printInfo() const {
-        std::cout << "Value a: " << a << std::endl;
-        std::cout << "Value b: " << b << std::endl;
-        std::cout << "Hourglass S: " << computeArea() << std::endl;
+        std::cout << "Value a: " << a << std::endl;  // Виведення значення "a"
+        std::cout << "Value b: " << b << std::endl;  // Виведення значення "b"
+        std::cout << "Hourglass S: " << computeArea() << std::endl;  // Виведення площі годинника
     }
 };
 
@@ -53,9 +54,9 @@ int main() {
     int b_value = sandglass.getB();
     int area = sandglass.computeArea();
 
-    std::cout << "Value a: " << a_value << std::endl;
-    std::cout << "Value b: " << b_value << std::endl;
-    std::cout << "Hourglass S: " << area << std::endl;
+    std::cout << "Value a: " << a_value << std::endl;  // Виведення значення "a"
+    std::cout << "Value b: " << b_value << std::endl;  // Виведення значення "b"
+    std::cout << "Hourglass S: " << area << std::endl;  // Виведення площі годинника
 
     // Присвоєння нових значень для a і b
     sandglass.setA(7);
